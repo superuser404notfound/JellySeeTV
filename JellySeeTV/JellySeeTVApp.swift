6a0caf2f66cdc26351b231b39ad8845339a69d1b
Vincent Herbst <superuser404@tuta.com>
2026-04-08 10:12:56 +0200

    Initial Commit

---
+++ JellySeeTV/JellySeeTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  JellySeeTVApp.swift
+//  JellySeeTV
+//
+//  Created by Vincent Herbst on 08.04.26.
+//
+
+import SwiftUI
+
+@main
+struct JellySeeTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
